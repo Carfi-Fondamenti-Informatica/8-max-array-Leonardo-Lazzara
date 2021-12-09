ef374bc8beab5a50c49f42f2abf7855fa0199bbe
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-12-09 22:46:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int n=0;
+    cout << "inserire la dimensione dell'array:" << endl;
+    cin >> n;
+
+    float A[n];
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    float risultato=max(A, n);
+    cout << risultato;
+
+    return 0;
 }
